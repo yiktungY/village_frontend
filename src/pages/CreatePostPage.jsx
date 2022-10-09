0be631e5a102ce2a19c7b2e8410783407d40d081
yiktungyeung <yiktungtony@gmail.com>
2022-10-09 11:41:03 -0400

    adding select and data picking

--- src/pages/CreatePostPage.jsx
+++ src/pages/CreatePostPage.jsx
@@ -1,11 +1,50 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
+import Select from "react-select";
+import makeAnimated from "react-select/animated";
+import DatePicker from "react-datepicker";
 
+import "react-datepicker/dist/react-datepicker.css";
 import { LoginButton, BasicInput, Button } from "../components/Elements";
 import UploadPicture from "../components/Users/UploadPicture";
 
+const options = [
+  { value: "Accounting", label: "Accounting" },
+  { value: "Babysitting", label: "Babysitting" },
+  { value: "Cook", label: "Cook" },
+  { value: "Housekeeping", label: "Housekeeping" },
+  {
+    value: "Design/ video editing",
+    label: "Design/ video editing",
+  },
+  { value: "Gardening", label: "Gardening" },
+  { value: "dog walking", label: "dog walking" },
+  { value: "technician jobs", label: "technician jobs" },
+  { value: "house moving", label: "house moving" },
+];
+
+const hourOptions = [
+  {
+    value: "less than 30mins",
+    label: "less than 5mins",
+  },
+  { value: "Hours", label: "Hours" },
+  { value: "0.5 - 1 hour", label: "0.5 - 1 hour" },
+  { value: "2 hours", label: "2 hours" },
+  { value: "3 - 5 hours", label: "3 - 5 hours" },
+  { value: "5 - 10 hours", label: "5 - 10 hours" },
+  { value: "Days", label: "Days" },
+  { value: "within 1 day", label: "within 1 day" },
+  { value: "1 - 3 days", label: "1 - 3 days" },
+  { value: "Weeks", label: "Weeks" },
+  { value: "within 1 week", label: "within 1 week" },
+  { value: "1 - 2 weeks", label: "1 - 2 weeks" },
+  { value: "Months", label: "Months" },
+];
+
 const CreatePostPage = () => {
+  const animatedComponents = makeAnimated();
   const [payMethodvalue, setpayMethodValue] = useState("Non-Monetary Payment");
   const [monPayMethod, setMonPayMethod] = useState(true);
   const [pictureUrl, setpictureUrl] = useState("");
@@ -96,7 +135,6 @@ const CreatePostPage = () => {
           />
           <BasicInput
             type="text"
-            id="title"
             label="*Title"
             // icon={!controllForm.password.error ? <BiLock /> : <BiErrorCircle />}
             error={controllForm.title}
@@ -109,10 +147,9 @@ const CreatePostPage = () => {
           />
           <BasicInput
             type="text"
-            id="content"
-            label="Content"
+            label="Details"
             // icon={!controllForm.password.error ? <BiLock /> : <BiErrorCircle />}
-            error={controllForm.content}
+            error={controllForm.title}
             handleOnChange={(e) =>
               handleChange(e, "title", isValidPassword(e.target.value))
             }
@@ -121,44 +158,20 @@ const CreatePostPage = () => {
             }
           />
 
-          <BasicInput
-            type="text"
-            id="title"
-            label="*Title"
-            // icon={!controllForm.password.error ? <BiLock /> : <BiErrorCircle />}
-            error={controllForm.title}
-            handleOnChange={(e) =>
-              handleChange(e, "title", isValidPassword(e.target.value))
-            }
-            handleOnBlur={(e) =>
-              handleError("title", isValidPassword(e.target.value))
-            }
+          <Select
+            closeMenuOnSelect={false}
+            components={animatedComponents}
+            defaultValue={options[4].value}
+            isMulti
+            options={options}
           />
-          <BasicInput
-            type="text"
-            id="title"
-            label="*Title"
-            // icon={!controllForm.password.error ? <BiLock /> : <BiErrorCircle />}
-            error={controllForm.title}
-            handleOnChange={(e) =>
-              handleChange(e, "title", isValidPassword(e.target.value))
-            }
-            handleOnBlur={(e) =>
-              handleError("title", isValidPassword(e.target.value))
-            }
-          />
-          <BasicInput
-            type="text"
-            id="title"
-            label="*Title"
-            // icon={!controllForm.password.error ? <BiLock /> : <BiErrorCircle />}
-            error={controllForm.title}
-            handleOnChange={(e) =>
-              handleChange(e, "title", isValidPassword(e.target.value))
-            }
-            handleOnBlur={(e) =>
-              handleError("title", isValidPassword(e.target.value))
-            }
+          <DatePicker selected={new Date()} />
+          <Select
+            closeMenuOnSelect={false}
+            components={animatedComponents}
+            defaultValue={hourOptions[0].value}
+            isMulti
+            options={hourOptions}
           />
           <Button
             action="Submit"
